7beda10956e57741b7478a060a360412a0f3fc6c
diamondburned <diamond@arikawa-hi.me>
2023-05-19 23:34:17 -0700

    Fix cleanup dist

--- build.ts
+++ build.ts
@@ -3,13 +3,13 @@ import * as esbuild from "https://deno.land/x/esbuild@v0.17.18/mod.js";
 import sass from "https://deno.land/x/denosass@1.0.6/mod.ts";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.7.0/mod.ts";
 
+await Deno.remove("dist", { recursive: true }).catch(() => {});
 await Deno.mkdir("dist", { recursive: true });
 
 const scssFile = await Deno.readTextFile("lib/webring.scss");
 // When will Deno-Rust wrapper libraries ever have actually nice function names?
 // Probably never lmfao. They couldn't even be bothered to wrap the functions?
 const css = sass(scssFile, { style: "compressed" }).to_string() as string;
-await Deno.remove("dist/webring.css", { recursive: true });
 await Deno.writeTextFile("dist/webring.css", css);
 
 await esbuild.build({
