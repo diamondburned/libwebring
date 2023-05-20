541ae8222acdb61f689f2a2322828a357a20b2ed
diamondburned <diamond@arikawa-hi.me>
2023-05-19 23:13:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# libwebring
+
+A tiny JavaScript library for adding a webring to your website.
+
+## Features
+
+- Vanilla JS with Web Components
+- No backend needed, just a simple raw GitHub link
+- Periodic health checking using GitHub Actions
+
+### Goals
+
+- Planned JS frameworks support
+  - Svelte
+- Be cute
+- Look cute
+- Low maintenance
+
+## Usage
+
+Add the script tag to your place. You may choose to use something like [jsdelivr](https://jsdelivr.com/) for this.
+
+Then, add the `<webring-element>` tag:
+
+```html
+<webring-element src="/path/to/webring.json" />
+```
+
+Make sure that your webring JSON either has the top-most domain name of your current page. For example, if you're on `b.libdb.so`, then `libdb.so` has to be on the list. Otherwise, use the `name` attribute:
+
+```html
+<webring-element name="diamond" src="/path/to/webring.json" />
+```
+
+### Example
+
+To run the example, run
+
+```sh
+deno task serve
+```
+
+Then head to http://localhost:4507/example/.
+
+## Customizing
+
+If `<webring-element>` is used without any children, it will generate a default HTML template to be used as the body. If it has existing children elements, then they will be kept.
+
+libwebring will look for the following classes to fill in the data:
+
+- `.ring` for the name of the webring (assumed to be an `<a>` tag)
+- `.left` for the left link (assumed to be an `<a>` tag)
+- `.middle` for the name of the current link
+- `.right` for the right link (assumed to be an `<a>` tag)
+
+To exclude websites that don't show the same webring, you can use the `exclude-missing-webring-sites` attribute:
+
+```html
+<webring-element
+  name="diamond"
+  src="/path/to/webring.json"
+  exclude-missing-webring-sites
+/>
+```
+
+By default, libwebring will assume that there is also a status JSON file next to the webring source JSON file. For example, if the webring source file is `webring`, then `webring.status.json` will also be checked.
+
+You can override the status source path by using the `status-src` attribute:
+
+```html
+<webring-element
+  name="diamond"
+  src="/path/to/webring.json"
+  status-src="/path/to/webring.status.json"
+/>
+```
+
+### Working without JavaScript
+
+libwebring can act as an enhancement for when there is JavaScript with a proper HTML fallback if there isn't. You do lose some of the convenience, but it will still work. To make it work without JavaScript, simply do what you would when customizing libwebring:
+
+```html
+<webring-element name="diamond" src="/path/to/webring.json">
+  <p>
+    <a class="left" href="://peekoe.net" target="_blank">peekoe</a>
+    <span class="middle">diamond</span>
+    <a class="right" href="://lieber.men" target="_blank">aaronlieb</a>
+  </p>
+</webring-element>
+```
+
+Without JavaScript, the browser will simply ignore the `<webring-element>` tag and render the fallback. With JavaScript, libwebring will kick in and reuse the existing elements to fill in the latest data.
---
+++ lib/webring.ts
@@ -0,0 +1,297 @@
+import { html } from "https://deno.land/x/literal_html@1.1.0/mod.ts";
+
+export type WebringData = {
+  readonly version: 1;
+  name?: string;
+  root?: string;
+  ring: WebringLink[];
+};
+
+export type WebringLink = {
+  name: string;
+  link: string;
+};
+
+export type WebringStatusData = {
+  readonly version: 1;
+  anomalies: Record<string, WebringLinkStatus>;
+};
+
+export type WebringLinkStatus = {
+  dead?: boolean;
+  missingWebring?: boolean;
+};
+
+export class Webring {
+  #data: WebringData | null = null;
+  #link: WebringLink | null = null; // as in chain link
+  #statusData: WebringStatusData | null = null;
+
+  constructor(
+    readonly src: string,
+    readonly opts: {
+      name?: string; // override the name of the link to use
+      statusSrc?: string;
+      data?: WebringData;
+      statusData?: WebringStatusData;
+      // excludeMissingWebringSites returns true if sites that don't have a
+      // webring link should be excluded from the webring. readonly
+      excludeMissingWebringSites?: boolean;
+    }
+  ) {
+    this.#data = opts.data || null;
+    this.#statusData = opts.statusData || null;
+  }
+
+  // surroundingLinks returns the links to the left and right of the current
+  // link, or null if there are no surrounding links.
+  surroundingLinks(): { left: WebringLink; right: WebringLink } | null {
+    if (!this.available) {
+      return null;
+    }
+
+    const ix = this.#data.ring.indexOf(this.#link);
+    if (ix == -1) {
+      return null;
+    }
+
+    const leftIx = (ix - 1 + this.#data.ring.length) % this.#data.ring.length;
+    const rightIx = (ix + 1) % this.#data.ring.length;
+
+    return {
+      left: this.#data.ring[leftIx],
+      right: this.#data.ring[rightIx],
+    };
+  }
+
+  get data(): WebringData {
+    return this.#data;
+  }
+
+  set data(data: WebringData) {
+    this.#data = data;
+    this.initLink();
+  }
+
+  get statusData(): WebringStatusData {
+    return this.#statusData;
+  }
+
+  set statusData(statusData: WebringStatusData) {
+    this.#statusData = statusData;
+  }
+
+  get link(): WebringLink | null {
+    return this.#link;
+  }
+
+  // available is true if the webring data is available.
+  get available(): boolean {
+    return !!this.#data && !!this.#link;
+  }
+
+  // update updates the webring data. It overrides the existing internal data.
+  async update() {
+    this.#data = null;
+    this.#statusData = null;
+    await this.init();
+  }
+
+  // includeLink returns true if the link should be included in the webring.
+  includeLink(link: WebringLink): boolean {
+    if (this.#statusData) {
+      const anomaly = this.#statusData.anomalies[link.link];
+      if (anomaly) {
+        if (anomaly.dead) {
+          return false;
+        }
+        if (this.opts.excludeMissingWebringSites && anomaly.missingWebring) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private async init() {
+    if (!this.#data) {
+      await this.initData(this.src);
+      this.initLink();
+    }
+
+    if (!this.#statusData) {
+      try {
+        await this.initStatusData(
+          this.opts.statusSrc || guessStatusSrc(this.src)
+        );
+      } catch (err) {
+        console.error("Failed to fetch status data", err);
+      }
+    }
+  }
+
+  private async initData(src: string) {
+    const response = await fetch(src);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch webring data: ${response.status}`);
+    }
+
+    this.#data = await response.json();
+    if (this.#data.version != 1) {
+      throw new Error(
+        `Unsupported webring format version: ${this.#data.version}`
+      );
+    }
+  }
+
+  private initLink() {
+    if (this.opts.name) {
+      this.#link = this.#data.ring.find((l) => l.name === this.opts.name);
+    } else {
+      const domain = window.location.host;
+      this.#link = this.#data.ring.find((l) => domainIncludes(l.link, domain));
+    }
+  }
+
+  private async initStatusData(statusSrc: string) {
+    const response = await fetch(statusSrc);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch status data: ${response.status}`);
+    }
+
+    this.#statusData = await response.json();
+    switch (this.#statusData.version) {
+      case 1: {
+        // Only keep links that are not in the unavailable list.
+        this.#data.ring = this.#data.ring.filter((l) => this.includeLink(l));
+        break;
+      }
+      default: {
+        throw new Error(
+          `Unsupported status format version: ${this.#statusData.version}`
+        );
+      }
+    }
+  }
+}
+
+export class WebringElement extends HTMLElement {
+  readonly webring: Webring;
+
+  constructor() {
+    super();
+
+    const src = this.attributes["src"]?.value;
+    if (!src) {
+      console.error("Not rendering webring: missing src attribute");
+      this.setVisible(false);
+      return;
+    }
+
+    this.webring = new Webring(src, {
+      name: this.attributes["name"]?.value,
+      statusSrc: this.attributes["status-src"]?.value,
+      excludeMissingWebringSites: this.hasAttribute(
+        "exclude-missing-webring-sites"
+      ),
+    });
+
+    if (elementIsEmpty(this)) {
+      this.innerHTML = html`
+        <p class="ring-info">
+          <a href="#" class="ring" target="_blank"></a> webring
+        </p>
+        <p class="ring-body">
+          <a class="left" href="#" target="_blank"></a>
+          <span class="middle"></span>
+          <a class="right" href="#" target="_blank"></a>
+        </p>
+      `;
+    }
+  }
+
+  connectedCallback() {
+    this.update();
+  }
+
+  // update asynchronously updates the webring data and re-renders the element.
+  update() {
+    this.webring.update().then(() => this.render());
+  }
+
+  private render() {
+    for (const k in this.dataset) {
+      delete this.dataset[k];
+    }
+
+    const surroundingLinks = this.webring.surroundingLinks();
+    if (!surroundingLinks) {
+      this.setVisible(false);
+      return;
+    }
+
+    this.dataset.ringName = this.webring.data.name;
+    this.dataset.linkName = this.webring.link.name;
+    this.dataset.leftLinkName = surroundingLinks.left.name;
+    this.dataset.rightLinkName = surroundingLinks.right.name;
+
+    this.$<HTMLAnchorElement>(".left", (a) => {
+      a.href = linkURL(surroundingLinks.left);
+      a.textContent = surroundingLinks.left.name;
+    });
+
+    this.$<HTMLAnchorElement>(".ring", (a) => {
+      a.href = this.webring.data.root;
+      a.textContent = this.webring.data.name || "";
+    });
+
+    this.$(".middle", (span) => {
+      span.textContent = this.webring.link.name || "";
+    });
+
+    this.$<HTMLAnchorElement>(".right", (a) => {
+      a.href = linkURL(surroundingLinks.right);
+      a.textContent = surroundingLinks.right.name;
+    });
+
+    this.setVisible(true);
+  }
+
+  private setVisible(visible: boolean) {
+    this.setAttribute("style", visible ? "" : "display: none;");
+  }
+
+  private $<T = HTMLElement>(selector: string, f: (_: T) => void) {
+    const el = this.querySelector(selector);
+    if (!el) {
+      throw new Error(`Element not found: ${selector}`);
+    }
+    f(el as T);
+  }
+}
+
+// domainIncludes returns true if matchingDomain is a subdomain of domain.
+function domainIncludes(domain: string, matchingDomain: string): boolean {
+  return domain == matchingDomain || matchingDomain.endsWith(`.${domain}`);
+}
+
+function linkURL(link: WebringLink): string {
+  if (link.link.includes("://")) {
+    return link.link;
+  }
+  return `https://${link.link}`;
+}
+
+function elementIsEmpty(el: HTMLElement): boolean {
+  return el.innerHTML.trim() == "";
+}
+
+function guessStatusSrc(src: string): string {
+  // webring.json -> webring.status.json
+  const parts = src.split(".");
+  parts[parts.length - 1] = "status." + parts[parts.length - 1];
+  return parts.join(".");
+}
+
+customElements.define("webring-element", WebringElement);
